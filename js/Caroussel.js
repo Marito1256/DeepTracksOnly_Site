916602dad02d4ad8606b792b43852f083482f972
Marito1256 <maritoespana@gmail.com>
2021-03-07 01:01:53 -0800

    Finally decided to push

---
+++ js/Caroussel.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    center:true,
+    nav:false,
+    dots: false,
+    
+    responsive:{
+        2000:{
+            items:4
+        }
+    }
+})
---
+++ js/Navbar.js
@@ -0,0 +1,5 @@
+const Navbutton = document.getElementsByClassName("Nav_button");
+console.log(Navbutton);
+Navbutton[0].addEventListener("click", function(e){
+    alert("gg");
+} , false);
